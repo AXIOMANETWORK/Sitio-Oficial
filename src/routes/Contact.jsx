44a60edfe2494dc2ad2afe1c9ae42d067571f517
EmmanuelRZar <emmanuelrzar@gmail.com>
2022-11-03 17:06:32 -0600

    Contact
    Se crea SCSS Contact
     Se importa en Global

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Header from '../components/Header'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Header title="Contact" info="Agency provides a full service range including technical skills, design, business understanding." />
+    <div className='Contact'>
+      
+      <section className='section-itemMap'>
+        <div classname='container-itemMap'>
+          <div className='container-map'>
+          </div>
+          <div className='container-form'>
+          </div>
+        </div>
+      </section>
+      
+    </div>
+
+    </>
   )
 }
 
